f0a9bb204a1d617cf6c2866cba410d1cfdf7d541
Rida Fatima <160426221+ridafatima1212@users.noreply.github.com>
2025-01-05 18:33:14 +0500

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from transformers import pipeline
 class SentimentModel:
     def __init__(self):
         # Using Hugging Face's pre-trained pipeline
-        self.analyzer = pipeline("sentiment-analysis")
+        self.analyzer = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
 
     def predict(self, text: str) -> str:
         # Get predictions
